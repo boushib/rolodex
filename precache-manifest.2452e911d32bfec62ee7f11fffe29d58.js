ae814c7766a9ce48d4a1ea61cbb70952d9836921
boushib <boushib01@gmail.com>
2019-09-29 22:02:09 +0100

    Updates

---
+++ precache-manifest.2452e911d32bfec62ee7f11fffe29d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e3c23727add401c95ee24c76ce541a",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "902772865296ab767271",
+    "url": "/rolodex/static/css/main.08f143e2.chunk.css"
+  },
+  {
+    "revision": "c725b4ee592336abd737",
+    "url": "/rolodex/static/js/2.82ffbe94.chunk.js"
+  },
+  {
+    "revision": "902772865296ab767271",
+    "url": "/rolodex/static/js/main.2bb0749b.chunk.js"
+  },
+  {
+    "revision": "775ebd40c664dd721aad",
+    "url": "/rolodex/static/js/runtime~main.ca9ef864.js"
+  }
+]);
